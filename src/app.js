b60d147cd4d40f18a206d866d5ee5b701e8d8e21
Greg Johnson <greg.johnson@boohoo.com>
2016-08-08 17:44:51 +0100

    Got babel react working

---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+ 
+React.render(
+  <HelloMessage name="John" />,
+  document.getElementById('orderhistory')
+);
